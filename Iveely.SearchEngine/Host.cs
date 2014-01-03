cf2e5f1000bed96b57789bc1f93864b1c4755c79
Fanping Liu <liufanping@iveely.com>
2014-01-03 22:27:05 +0800

    搜索整理

--- Iveely.SearchEngine/Host.cs
+++ Iveely.SearchEngine/Host.cs
@@ -23,93 +23,27 @@ namespace Iveely.SearchEngine
 {
     public class Host : Application
     {
+        private Library library = new Library();
+
         public static void Main()
         {
             Backstage backstage = new Backstage();
             backstage.Run(new object[] { 8001, 8001, 8001, 8001, 8001, 8001 });
-            //const string delimiter = ".?。！\t？…●|\r\n])!";
-            //string[] sentences = File.ReadAllText("TestData.txt", Encoding.UTF8).Split(delimiter.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-            //foreach (string sentence in sentences)
-            //{
-            //    if (sentence.Length >= 5)
-            //    {
-            //        Template.Question result = Bot.GetInstance("Init\\").BuildQuestion(sentence, "http://", "tutl");
-            //        if (result.Description != null)
-            //        {
-            //            foreach (var question in result.Description)
-            //            {
-            //                Console.WriteLine(sentence);
-            //                Console.WriteLine(question.Item1 + "? " + question.Item2);
-            //                Console.WriteLine();
-            //            }
-            //        }
-            //    }
-            //}
             Console.ReadKey();
-            //Host host = new Host();
-            //host.Run(null);
         }
 
         public override void Run(object[] args)
         {
-            IEnumerable<string> workers = GetAllWorkers();
-            var enumerable = workers as string[] ?? workers.ToArray();
-            Console.WriteLine("Get Worker count:" + enumerable.Count());
-            if (workers != null)
+            while (true)
             {
-                while (true)
-                {
-                    Console.Write("Enter your query:");
-                    string query = Console.ReadLine();
-                    string result = GetGlobalCache<string>(query);
-                    Console.WriteLine("query:[{0}] in cache is {1}", query, result);
-                    if (string.IsNullOrEmpty(result))
-                    {
-                        string timestamp = DateTime.UtcNow.ToLongDateString();
-                        SetGlobalCache("CurrentQueryIndex", timestamp);
-                        SetGlobalCache(timestamp, query);
-                        int sendIndex = 9000;
-                        List<string> cacheStore = new List<string>();
-                        foreach (string worker in enumerable)
-                        {
-                            string[] workerInfo = worker.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                            int endFlagIndex = workerInfo[0].LastIndexOf("/", StringComparison.Ordinal) + 1;
-                            string ip = workerInfo[0].Substring(endFlagIndex, workerInfo[0].Length - endFlagIndex);
+                Console.Write("Text Query Words:");
+                string query = Console.ReadLine();
+                Console.WriteLine(library.TextQuery(query));
 
-                            try
-                            {
-                                sendIndex += (int.Parse(workerInfo[1]) % 100);
-                                Client client = new Client(ip, sendIndex);
-                                Packet dataPacket = new Packet(new byte[1]) { WaiteCallBack = true };
-                                client.Send<bool>(dataPacket);
-                                cacheStore.Add(ip + "," + sendIndex);
-                                sendIndex = 9000;
-                            }
-                            catch (Exception exception)
-                            {
-                                Console.WriteLine(exception);
-                            }
-                        }
-                        Thread.Sleep(2000);
-                        foreach (string ca in cacheStore)
-                        {
-                            try
-                            {
-                                string outputResult = ca + query;
-                                Console.WriteLine(outputResult);
-                                result += GetGlobalCache<string>(outputResult);
-                            }
-                            catch (Exception exception)
-                            {
-                                Console.WriteLine(exception);
-                            }
-
-                        }
-                    }
-                    Console.WriteLine("Finnal result :" + result);
-                }
+                Console.Write("Relative Query Word:");
+                query = Console.ReadLine();
+                Console.WriteLine(library.RelativeQuery(query));
             }
-            Console.WriteLine("Not found any workers!");
         }
     }
 }
