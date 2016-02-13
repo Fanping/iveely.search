80e934552926171665b15cadb03d2802c4b9f473
Fanping Liu <liufanping@iveely.com>
2016-02-13 17:38:40 +0800

    use maven.

---
+++ src/Program.java
@@ -0,0 +1,25 @@
+/**
+ * write  date: 2016Äê2ÔÂ13ÈÕ
+ * author name: Iveely Liu
+ * contact  me: sea11510@mail.ustc.edu.cn
+ * description:
+ */
+
+/**
+* @description 
+* @author Iveely Liu
+*/
+/**
+ * 
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
